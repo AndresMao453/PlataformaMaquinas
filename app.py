ff3dadf99de0ce7125e58ce389daeddcbab6a8e4
Mauricio <mauricio.onate@fasecol.com>
2026-02-17 10:48:44 -0500

    SAPE

--- app.py
+++ app.py
@@ -2604,7 +2604,6 @@ def run_analysis():
 
 
 if __name__ == "__main__":
-    import webbrowser
-    webbrowser.open("http://127.0.0.1:5000")
-    app.run(debug=False, host="127.0.0.1", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
 
